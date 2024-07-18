6ea6e5da1dac8a4e62b6851c5c530f995e4df525
chaosll <1061813923@qq.com>
2024-07-18 17:18:23 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+    <div className='flex'>
+      <div className="bg-[#f0f] dark:bg-[#f00] w-1/2 h-screen text-black dark:text-white">1</div>
+      <div className="text-red-600">2</div>
+    </div>
+    </>
+  )
+}
+
+export default App
